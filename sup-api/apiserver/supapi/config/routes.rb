f604db65e19b3e72499e8db65d08e8b7b40040ed
sivertigo <sg.tak22@gmail.com>
2017-10-09 11:19:27 +0900

    first commit

--- sup-api/apiserver/supapi/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :records
-  resources :players, param: :player_id
-  namespace :api, {format: 'json'} do
-    namespace :v1 do
-       resources :players, param: :player_id
-    end
-  end
-end
